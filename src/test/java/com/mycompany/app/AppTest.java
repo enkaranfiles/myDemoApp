24b628eb75dbbba8495b0e9597ff7e59c91d7e89
enkaranfiles <enkaranfiles@gmail.com>
2018-10-09 21:59:17 +0300

    some fixing

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -42,21 +42,38 @@ public class AppTest
     }*/
       public void testFound() {
       ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertTrue(new App().search(array, 4));
+      ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(4, 3, 2, 1));
+      assertTrue(new App().myMethod(array,array2,4,4));
     }
 
     public void testNotFound() {
       ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertFalse(new App().search(array, 5));
+      ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+      int result=array.size();
+      int result2=array.size();
+      
+      assertFalse(new App().myMethod(array,array2,result,result2));
     }
 
     public void testEmptyArray() {
       ArrayList<Integer> array = new ArrayList<>();
-      assertFalse(new App().search(array, 1));
+      ArrayList<Integer> array2 = new ArrayList<>();
+
+      assertFalse(new App().myMethod(array,array2,0,0));
     }
 
     public void testNull() {
-      assertFalse(new App().search(null, 1));
+      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+      assertFalse(new App().myMethod(array,null,0,3));
+    }
+
+    public void testSize(){
+      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3));
+      ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(8,5,3));
+      int result=array.size();
+      int result2=array.size();
+      
+      assertFalse(new App().myMethod(array,array2,result,result2));
     }
 
 }
